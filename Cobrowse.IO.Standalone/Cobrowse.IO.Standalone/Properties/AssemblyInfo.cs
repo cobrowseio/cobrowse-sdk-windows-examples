7556935b3de620e918154cd052f6608cce3f8e37
preseverence <rattler@brokenevent.com>
2020-06-19 22:23:33 +0300

    fixed assemblyinfo

--- Cobrowse.IO.Standalone/Cobrowse.IO.Standalone/Properties/AssemblyInfo.cs
+++ Cobrowse.IO.Standalone/Cobrowse.IO.Standalone/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cobrowse.IO.Screenshare")]
+[assembly: AssemblyTitle("Cobrowse.IO.Standalone")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cobrowse.IO.Screenshare")]
+[assembly: AssemblyProduct("Cobrowse.IO.Standalone")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
